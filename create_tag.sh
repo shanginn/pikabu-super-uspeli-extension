8e4c26eaabbf38d4ea4ff187d2452a3a9c7a12ae
Nikolai Shangin <shanginn@gmail.com>
2023-04-29 21:43:07 +0800

    Create and push new tag 'vX.X.X' and update package.json and manifest.json files with new version.

--- create_tag.sh
+++ create_tag.sh
@@ -11,6 +11,8 @@ if [ -z "$tag_exists" ]; then
   echo "Tag 'v$version' does not exist. Creating it now."
   git tag -a "v$version" -m "Create tag 'v$version' from script"
   echo "Tag 'v$version' created."
+  echo "Pushing tag 'v$version' to the remote repository."
+  git push origin "v$version"
 else
   echo "Tag 'v$version' already exists."
 fi
